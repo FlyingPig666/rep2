98cc1245c52a20a80a66693748d0945564d9a10d
FlyingPig666 <45942463+FlyingPig666@users.noreply.github.com>
2019-01-16 22:19:25 +0800

    Update UserServiceImpl.java
    修改了service实现类

--- myspring_day01/src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ myspring_day01/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -5,7 +5,7 @@ import com.itheima.service.UserService;
 public class UserServiceImpl implements UserService{
 
     public UserServiceImpl(){
-        System.out.println("对象创建啦!");
+        System.out.println("对象创建啦啦啦!");
     }
 
 }
